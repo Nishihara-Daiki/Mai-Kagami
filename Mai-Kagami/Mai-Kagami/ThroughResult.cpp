69335f95bbb693f14cebe21816b111be7e6a25cf
Nishihara-Daiki <e1227@s.akashi.ac.jp>
2017-02-13 01:07:37 +0900

    ImagineCup動画作成用

--- Mai-Kagami/Mai-Kagami/ThroughResult.cpp
+++ Mai-Kagami/Mai-Kagami/ThroughResult.cpp
@@ -5,13 +5,13 @@ ThroughResult::ThroughResult(Font *font, Songs *songs, Touch *touch) {
 	title        = new MyDrawTextLine(font, "採点結果", WIDTH * 0.5, 
 									  HEIGHT * 0.15, 1, 60, WIDTH * 0.5, 4);						//採点結果
 	circle       = new MyDrawCircle(WIDTH * 0.5, HEIGHT * 0.5, WIDTH * 0.3, 10, "WHITE");			//縁が白色の円
-	pointCircle  = new MyDrawCircleGauge(WIDTH * 0.5, HEIGHT * 0.5, WIDTH * 0.3, 58, 6);			//青色の弧
+	pointCircle  = new MyDrawCircleGauge(WIDTH * 0.5, HEIGHT * 0.5, WIDTH * 0.3, 82, 6);			//青色の弧
 	pointCircle2 = new MyDrawCircle(pointCircle->GetEndX() * SIZE_RATE, 
 									pointCircle->GetEndY() * SIZE_RATE, 16);						//弧の先の円
 	button       = new CircleButton2(font, touch, "次へ", 4);										//次へボタン
 	text         = new MyDrawText(font, "総合得点", WIDTH * 0.5, HEIGHT * 0.42, 1, 46, "White");	//「総合得点」
-	point1       = new MyDrawGraph(WIDTH * 0.42, HEIGHT * 0.51, "img/5.png", 0.23);					//得点(十の位)	
-	point2       = new MyDrawGraph(WIDTH * 0.51, HEIGHT * 0.51, "img/8.png", 0.23);					//得点(一の位)
+	point1       = new MyDrawGraph(WIDTH * 0.42, HEIGHT * 0.51, "img/8.png", 0.23);					//得点(十の位)	
+	point2       = new MyDrawGraph(WIDTH * 0.51, HEIGHT * 0.51, "img/2.png", 0.23);					//得点(一の位)
 	unit         = new MyDrawText(font, "点", WIDTH * 0.58, HEIGHT * 0.54, 0, 46, "White");			//「点」
 	last         = new MyDrawText(font, "前回 --点", WIDTH * 0.5, HEIGHT * 0.58, 1, 36, "White");	//前回の点数
 }
